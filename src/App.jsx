a44bbeb4b93e4e904599b215d04dbcfe68f3f8fd
Anchal Jain <anchal6904@gmail.com>
2025-03-27 21:32:55 +0530

    Updated feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { Routes, Route } from "react-router-dom";
-import Homepage from "./pages/Homepage";
-import MoviePage from "./pages/MoviePage";
-import PlaysPage from "./pages/PlaysPage";
+import Homepage from "./pages/Homepage.jsx";
+import MoviePage from "./pages/MoviePage.jsx";
+import PlaysPage from "./pages/PlaysPage.jsx";
 import axios from 'axios';
 
 axios.defaults.baseURL = 'https://api.themoviedb.org/3';
